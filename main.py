c306ccacf4728436a8af136039ebb78cfc587bca
Albert Lewis <45643396+BertieMcBergensen@users.noreply.github.com>
2020-03-05 09:38:37 -0500

    work on move function
    Add way to calculate position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import math
 import string
 
 GENES = '''udlr'''
@@ -6,7 +7,7 @@ GENES = '''udlr'''
 map = [ [0,0,0,1,'x'], 
         [1,1,0,0,0],        
         [0,1,1,0,1],
-        ['s',0,0,1,1]]
+        ['s',0,0,0,1]]
 
 target_path = 'rruuru'
 
@@ -14,16 +15,16 @@ class Individual(object):
 
     def __init__(self):
         self.chromosome = self.create_chromosome()
+
         # Find starting spot 's'
         for i in range(len(map)):
             for j in range(len(map[0])):
                 if map[i][j] == 's':
                     self.positionX = j
-                    self.positionY = i  
-                    
+                    self.positionY = i     
+
         self.fitness = self.calculate_fitness()
 
-        
 
     # Returns the individual's fitness (closest distance to ending point)
     def calculate_fitness(self):
@@ -33,10 +34,9 @@ class Individual(object):
                 if map[i][j] == 'x':
                     goalX = j
                     goalY = i
-        print(goalX)
-        print(goalY)
-        difference = (goalX - self.positionX, goalY-self.positionY)
-        print(difference)
+
+        difference = math.sqrt((goalX - self.positionX)**2 + (goalY-self.positionY)**2)
+        return difference
 
     # Creates a chromosome
     def create_chromosome(self):  
@@ -54,25 +54,38 @@ class Individual(object):
     def mutate(self):
         self.chromosome[random.randint(0, len(self.chromosome)-1)] = random.choice(GENES)
 
-
     def move(self):
 
-        # for movement in range(len(chromosome)):
-        #     if movement == 'u':
-        #         self.positiony -= 1
-        #     if movement == 'd':
-        #         self.positiony += 1
-        #     if movement == 'l':
-        #         self.positionx -= 1
-        #     if movement == 'r':
-        #         self.positionx += 1
+            for movement in self.chromosome:
+                print('Position x: ' + str(self.positionX))
+                print('Position y: ' + str(self.positionY))
+                print()
+                if 0 <= self.positionX <= 4 and 0 <= self.positionY <= 3:  
+                    moveX = self.positionX
+                    moveY = self.positionY
 
-        return "s"
+                    if movement == 'u':
+                        moveY -= 1
+                    elif movement == 'd':
+                        moveY += 1
+                    elif movement == 'l':
+                        moveX -= 1
+                    elif movement == 'r':
+                        moveX += 1
+
+                    if 0 <= moveX <= 4 and 0 <= moveY <= 3 and map[moveY][moveX] != 1:
+                        self.positionX = moveX
+                        self.positionY = moveY
+
 
 def main():
 
     xd = Individual()
     xd.mutate()
+    print(xd.chromosome)
+    print(xd.calculate_fitness())
+    xd.move()
+    print(xd.calculate_fitness())
 
     # Print map
     print('Map:')
