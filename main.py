718b7a6a2a9237a80b2fd33e08cce494daeeb1d4
Albert Lewis <45643396+BertieMcBergensen@users.noreply.github.com>
2020-03-10 10:37:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,39 +46,65 @@ class Individual(object):
             chromosome += random.choice(GENES)
 
         return chromosome
-
-    # def mate(self):
-
-        
+      
     # Chance to add random gene into chromosome
     def mutate(self):
         self.chromosome[random.randint(0, len(self.chromosome)-1)] = random.choice(GENES)
 
     def move(self):
 
-            for movement in self.chromosome:
-                print('Position x: ' + str(self.positionX))
-                print('Position y: ' + str(self.positionY))
-                print()
-                if 0 <= self.positionX <= 4 and 0 <= self.positionY <= 3:  
-                    moveX = self.positionX
-                    moveY = self.positionY
+        map[self.positionY][self.positionX] = 0
 
-                    if movement == 'u':
-                        moveY -= 1
-                    elif movement == 'd':
-                        moveY += 1
-                    elif movement == 'l':
-                        moveX -= 1
-                    elif movement == 'r':
-                        moveX += 1
+        for movement in self.chromosome:
+            print('Position x: ' + str(self.positionX))
+            print('Position y: ' + str(self.positionY))
+            print()
 
-                    if 0 <= moveX <= 4 and 0 <= moveY <= 3 and map[moveY][moveX] != 1:
-                        self.positionX = moveX
-                        self.positionY = moveY
+            if 0 <= self.positionX <= 4 and 0 <= self.positionY <= 3:  
+                moveX = self.positionX
+                moveY = self.positionY
+
+                if movement == 'u':
+                    moveY -= 1
+                elif movement == 'd':
+                    moveY += 1
+                elif movement == 'l':
+                    moveX -= 1
+                elif movement == 'r':
+                    moveX += 1
+
+                if 0 <= moveX <= 4 and 0 <= moveY <= 3 and map[moveY][moveX] != 1:
+                    self.positionX = moveX
+                    self.positionY = moveY
+
+        map[self.positionY][self.positionX] = 's'
+
+def mate(individual1, individual2):
+
+    child_chromosome = []
+
+    for gene_index in range(len(individual1.chromosome)):
+        if random.randint(1,100) <= 50:
+            child_chromosome.append(individual1.chromosome[gene_index])
+        else:
+            child_chromosome.append(individual2.chromosome[gene_index])
+
+    return child_chromosome
 
 
 def main():
+
+    ''' create a set of individuals
+    loop through the set of individuals
+    get the best and have them mate until they reach the end
+    print out the best individual from each generation
+    '''
+
+    bruh1 = Individual()
+    bruh2 = Individual()
+    print("bruh1" + str(bruh1.chromosome))
+    print("bruh2" + str(bruh2.chromosome))
+    print("child" + str(mate(bruh1,bruh2)))
 
     xd = Individual()
     xd.mutate()
@@ -97,4 +123,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
