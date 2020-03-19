b469a005e6a3bab496cc5d4653d28bf94ff6c0bb
BertieMcBergensen <45643396+BertieMcBergensen@users.noreply.github.com>
2020-03-19 19:34:46 -0400

    Finish main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import string
 
 GENES = '''udlr'''
 
+population_size = 50
+
 map = [ [0,0,0,1,'x'], 
         [1,1,0,0,0],        
         [0,1,1,0,1],
         ['s',0,0,0,1]]
 
-target_path = 'rruuru'
+target_path = 'rrruuru'
 
 class Individual(object):
 
@@ -25,9 +27,7 @@ class Individual(object):
                 if map[i][j] == 's':
                     self.positionX = j
                     self.positionY = i     
-
-        self.fitness = self.calculate_fitness()
-
+        self.fitness = 10
 
     # Returns the individual's fitness (closest distance to ending point)
     def calculate_fitness(self):
@@ -35,8 +35,9 @@ class Individual(object):
         for i in range(len(map)):
             for j in range(len(map[0])):
                 if map[i][j] == 'x':
-                    goalX = j
+                    
                     goalY = i
+                    goalX = j
 
         difference = math.sqrt((goalX - self.positionX)**2 + (goalY-self.positionY)**2)
         return difference
@@ -95,32 +96,59 @@ def mate(individual1, individual2):
 
     return child_chromosome
 
-
 def main():
 
-    ''' create a set of individuals
-    loop through the set of individuals
-    get the best and have them mate until they reach the end
-    print out the best individual from each generation
-    '''
+    # create initial population
+    population = []
 
-    deven = Individual()
-    print("deven " + str(deven.chromosome))
+    for _ in range(population_size):
+        population.append(Individual())
 
-    bruh1 = Individual()
-    bruh2 = Individual()
-    print("bruh1" + str(bruh1.chromosome))
-    print("bruh2" + str(bruh2.chromosome))
-    print("child" + str(mate(bruh1,bruh2)))
+    for individual in population:
+        individual.move()
+    
+    population.sort(key=lambda individual: individual.fitness)
+    
+    print("Generation 0")
+    for individual in population:
+            print(individual.fitness)
 
-    xd = Individual()
-    xd.mutate()
-    print(xd.chromosome)
-    print(xd.calculate_fitness())
-    xd.move()
-    print(xd.calculate_fitness())
 
-    # Print map
+    # while the best member of the population does not reach the goal, keep mating
+    gen = 0
+    while population[0].fitness != 0:
+     
+        gen += 1
+        # if gen > 3:
+        #     break
+
+        print('Generation', gen)
+        new_population = []
+
+        for top_individual in population[0:int(population_size/10)]:  
+                new_population.append(top_individual)
+
+        while len(new_population) <= population_size:
+            new_population.append(Individual(mate(population[0],population[1])))
+
+        for individual in new_population:
+
+            if random.randint(1,10) <= 2: # mutation chance 30%
+                individual.mutate()
+
+            individual.move()
+            individual.fitness = individual.calculate_fitness()
+
+        population = new_population
+        population.sort(key=lambda individual: individual.fitness)
+
+        for individual in population:
+            print(individual.fitness)
+
+    print('done')
+    print('winning fitness', population[0].fitness)
+    print('winning chromosome', population[0].chromosome)
+    
     print('Map:')
     for i in range(len(map)):     
         for j in range(len(map[0])):
